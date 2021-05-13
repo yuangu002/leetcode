13b964a45547bce02789ec9d1c685b4b35e8edb7
Yuan Gu <guy@andrew.cmu.edu>
2021-05-13 10:28:41 -0400

    hello

--- BSTiterator.java
+++ BSTiterator.java
@@ -36,3 +36,15 @@ public class BSTiterator {
         }
     }
 }
+
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+
+    public TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
--- FourSum.java
+++ FourSum.java
@@ -1,6 +1,49 @@
-import java.util.List;
+import java.util.*;
 
 public class FourSum {
     class Solution {
+        public List<List<Integer>> fourSum(int[] numbers, int target) {
+            // write your code here
+            Arrays.sort(numbers);
+            List<List<Integer>> res = new ArrayList<>();
+            if (numbers == null || numbers.length < 4) {
+                return res;
+            }
+            
+            for (int i = 0; i < numbers.length - 3; ++i) {
+                if (i > 0 && numbers[i] == numbers[i - 1]) {
+                    continue;
+                }
+                for (int j = i + 1; j < numbers.length - 2; ++j) {
+                    if (j > i + 1 && numbers[j] == numbers[j - 1]) {
+                        continue;
+                    }
+                    int left = j + 1, right = numbers.length - 1, newtarget = target - (numbers[i] + numbers[j]);
+                    while (left < right) {
+                        if (numbers[left] + numbers[right] == newtarget) {
+                            ArrayList<Integer> quadruplets = new ArrayList<>();
+                            quadruplets.add(numbers[i]);
+                            quadruplets.add(numbers[j]);
+                            quadruplets.add(numbers[left]);
+                            quadruplets.add(numbers[right]);
+                            res.add(quadruplets);
+                            ++left;
+                            --right;
+                            while (left < right && numbers[left] == numbers[left - 1]){
+                                ++left;
+                            }
+                            while (left < right && numbers[right] == numbers[right + 1]) {
+                                --right;
+                            }
+                        } else if (numbers[left] + numbers[right] < newtarget) {
+                            ++left;
+                        } else {
+                            --right;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
     }
 }
