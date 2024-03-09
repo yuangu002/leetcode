1e09559b6306e73d8cea2d09f7f44af026cf514e
Yuan Gu <guyuan002@gmail.com>
2024-03-09 03:00:03 -0800

    design tic-tac-toc and leader board

---
+++ DesignLeaderboard.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Leaderboard {
+    TreeMap<Integer, Integer> score2Count;
+    Map<Integer, Integer> player2Score;
+    public Leaderboard() {
+        score2Count = new TreeMap<Integer, Integer>(Collections.reverseOrder());
+        player2Score = new HashMap<Integer, Integer>();
+    }
+    
+    public void addScore(int playerId, int score) {
+        int preScore = player2Score.getOrDefault(playerId, 0);
+        int newScore = preScore + score;
+        player2Score.put(playerId, newScore);
+        if (preScore > 0 && score2Count.containsKey(preScore)) {
+            score2Count.put(preScore, score2Count.get(preScore) - 1);
+        }
+        score2Count.put(newScore, score2Count.getOrDefault(newScore, 0) + 1);
+    }
+    
+    public int top(int K) {
+        int cnt = 0;
+        int sum = 0;
+        for (int score: score2Count.keySet()) {
+            int freq = score2Count.get(score);
+            int len = freq;
+
+            while (len > 0) {
+                sum += score;
+                cnt++;
+                if (cnt == K) {
+                    return sum;
+                }
+                len--;
+            }
+        }
+        return sum;
+    }
+    
+    public void reset(int playerId) {
+        int score = player2Score.get(playerId);
+        player2Score.remove(playerId);
+        score2Count.put(score, score2Count.get(score) - 1);
+    }
+}
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard obj = new Leaderboard();
+ * obj.addScore(playerId,score);
+ * int param_2 = obj.top(K);
+ * obj.reset(playerId);
+ */
---
+++ TicTacToc.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class TicTacToe {
+    Map<String, Set<Integer>> player1;
+    Map<String, Set<Integer>> player2;
+    int n;
+
+    public TicTacToe(int n) {
+        player1 = new HashMap<>();
+        player2 = new HashMap<>();
+        this.n = n;
+    }
+    
+    public int move(int row, int col, int player) {
+        if (player == 1) {
+            return process(player1, row, col, player);
+        }
+        return process(player2, row, col, player);
+    }
+
+    private int process(Map<String, Set<Integer>> curMap, int row, int col, int player) {
+        Set<Integer> rowSet = curMap.getOrDefault("row: " + row, new HashSet<Integer>());
+        rowSet.add(col);
+        if (rowSet.size() == n) {
+            return player;
+        }
+        curMap.put("row: " + row, rowSet);
+        Set<Integer> colSet = curMap.getOrDefault("col: " + col, new HashSet<Integer>());
+        colSet.add(row);
+        if (colSet.size() == n) {
+            return player;
+        }
+        curMap.put("col: " + col, colSet);
+        if (row == col) {
+            Set<Integer> diagonalSet = curMap.getOrDefault("diagonal", new HashSet<Integer>());
+            diagonalSet.add(row * n + col);
+            if (diagonalSet.size() == n) {
+                return player;
+            }
+            curMap.put("diagonal", diagonalSet);
+        }
+        if (row + col == n - 1) {
+            Set<Integer> antiDiagonalSet = curMap.getOrDefault("anti-diagonal", new HashSet<Integer>());
+            antiDiagonalSet.add(row * n + col);
+            if (antiDiagonalSet.size() == n) {
+                return player;
+            }
+            curMap.put("anti-diagonal", antiDiagonalSet);
+        }
+        return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
